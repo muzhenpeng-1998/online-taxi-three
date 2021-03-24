6f6e790d0b365a557f0e66c82b503d777f42d2fd
muzhenpeng <zhiwei741@icloud.com>
2021-03-24 21:52:54 +0800

    添加日志

--- service-verification-code/src/main/java/com/golike/serviceverificationcode/controller/VerifyCodeController.java
+++ service-verification-code/src/main/java/com/golike/serviceverificationcode/controller/VerifyCodeController.java
@@ -2,6 +2,7 @@ package com.golike.serviceverificationcode.controller;
 
 import com.golike.internalcommon.dto.ResponseResult;
 import com.golike.serviceverificationcode.service.VerifyCodeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/verify-code")
+@Slf4j
 public class VerifyCodeController {
 
     @Autowired
@@ -26,7 +28,7 @@ public class VerifyCodeController {
      */
     @GetMapping("/generate/{identity}/{phoneNumber}")
     public ResponseResult generate(@PathVariable("identity") int identity , @PathVariable ("phoneNumber") String phoneNumber) {
-//        log.info("/generate/{identity}/{phoneNumber} ： 身份类型："+identity+",手机号："+phoneNumber);
+        log.info("/generate/{identity}/{phoneNumber} ： 身份类型："+identity+",手机号："+phoneNumber);
         // 校验参数
 
         return verifyCodeService.generate(identity,phoneNumber);
