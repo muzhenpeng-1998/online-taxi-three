13b34fd2107fed78d9182e14f4c6ff7c6a8340f9
muzhenpeng <zhiwei741@icloud.com>
2021-03-24 20:28:50 +0800

    网约车项目初始化

---
+++ cloud-eureka/src/main/java/com/golike/cloudeureka/CloudEurekaApplication.java
@@ -0,0 +1,18 @@
+package com.golike.cloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhenpengmu
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaApplication.class, args);
+	}
+
+}
---
+++ cloud-eureka/src/test/java/com/golike/cloudeureka/CloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golike.cloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
