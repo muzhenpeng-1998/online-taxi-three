87ebd4ab12be581a8408a0baa255ffb7108ed19f
muzhenpeng <zhiwei741@icloud.com>
2021-03-24 21:46:48 +0800

    生成验证码

---
+++ internal-common/src/main/java/com/golike/internalcommon/dto/serviceverificationcode/response/VerifyCodeResponse.java
@@ -0,0 +1,12 @@
+package com.golike.internalcommon.dto.serviceverificationcode.response;
+
+import lombok.Data;
+
+/**
+ * @author yueyi2019
+ */
+@Data
+public class VerifyCodeResponse {
+	
+	private String code;
+}
---
+++ service-verification-code/src/main/java/com/golike/serviceverificationcode/controller/VerifyCodeController.java
@@ -0,0 +1,35 @@
+package com.golike.serviceverificationcode.controller;
+
+import com.golike.internalcommon.dto.ResponseResult;
+import com.golike.serviceverificationcode.service.VerifyCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhenpengmu
+ */
+@RestController
+@RequestMapping("/verify-code")
+public class VerifyCodeController {
+
+    @Autowired
+    private VerifyCodeService verifyCodeService;
+
+    /**
+     * 根据身份，手机号，生成验证码
+     * @param identity
+     * @param phoneNumber
+     * @return
+     */
+    @GetMapping("/generate/{identity}/{phoneNumber}")
+    public ResponseResult generate(@PathVariable("identity") int identity , @PathVariable ("phoneNumber") String phoneNumber) {
+//        log.info("/generate/{identity}/{phoneNumber} ： 身份类型："+identity+",手机号："+phoneNumber);
+        // 校验参数
+
+        return verifyCodeService.generate(identity,phoneNumber);
+    }
+
+}
