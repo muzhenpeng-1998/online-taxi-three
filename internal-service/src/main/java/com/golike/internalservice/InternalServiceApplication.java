d033684be37a606e19377b93e0131721cde2df91
muzhenpeng <zhiwei741@icloud.com>
2021-03-27 23:25:18 +0800

    增加internal-service服务

---
+++ internal-service/src/main/java/com/golike/internalservice/InternalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.golike.internalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalServiceApplication.class, args);
+    }
+
+}
---
+++ internal-service/src/test/java/com/golike/internalservice/InternalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golike.internalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
